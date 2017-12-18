370432d06f32245d4babcfe4f6c64116f69df946
Szymon Kaliski <hi@szymonkaliski.com>
2017-12-18 15:37:20 +0100

    stl + gcode

--- CaseModeling/export.js
+++ CaseModeling/export.js
@@ -4,4 +4,4 @@ const fs = require("fs");
 
 const rawData = stlSerializer.serialize(model, { binary: false });
 
-fs.writeFileSync("monome-case.stl", rawData.join());
+fs.writeFileSync("data/monome-case.stl", rawData.join());
