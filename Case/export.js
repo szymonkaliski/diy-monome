6a83f04b1bc453c43e80068e46fe2397573c11d9
Szymon Kaliski <hi@szymonkaliski.com>
2017-12-26 12:17:34 +0100

    export different case files

--- Case/export.js
+++ Case/export.js
@@ -1,6 +1,14 @@
+if (!process.argv[2]) {
+  console.log("pass model.js script path as argument to export to stl");
+  process.exit(0);
+}
+
+const path = require("path");
+const modelScriptPath = `./${path.basename(process.argv[2])}`;
+
 const { CSG } = require("@jscad/csg");
 const { stlSerializer } = require("@jscad/io");
-const { model } = require("./model");
+const { model } = require(modelScriptPath);
 const fs = require("fs");
 
 const MM = 10;
@@ -11,4 +19,12 @@ const scaledModel = model
 
 const rawData = stlSerializer.serialize(scaledModel, { binary: false });
 
-fs.writeFileSync("data/monome-case.stl", rawData.join());
+const stlName = `${path.basename(
+  modelScriptPath,
+  path.extname(modelScriptPath)
+)}.stl`;
+const stlPath = `data/${stlName}`;
+
+fs.writeFileSync(stlPath, rawData.join());
+
+console.log(`exported ${path.basename(modelScriptPath)} to ${stlPath}`);
