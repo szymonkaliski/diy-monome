78c4f3e1e39a78134dc01a6b65adaa05f58e26c2
okyeron <nevetsokyeron@NyanNyan.local>
2018-07-28 16:40:32 -0500

    removed non-trellis files

--- README.md
+++ README.md
@@ -1,45 +1,29 @@
-# DIY Monome
+# itsagrid 64/128 - a DIY monome compatible grid
 
-This repo contains *some* tools for building and running DIY version of [monome](https://monome.org/) 8x8 grid, that's *mostly* compatible with existing monome patches and software.
+A DIY version of [monome](https://monome.org/) 8x8 and 16x8 grid instruments, that are  *mostly* compatible with existing monome patches and software.
 
-Trellinome updated for 128 (16x8 grid) and for other configurations like 4x8 or 4x16 grids.
+itsagrid is an adaptation of the 
+[Adafruit Trellis board](https://www.adafruit.com/product/1616) with the a rubber button grid from Livid Instruments (no longer made by Livid) and using a [Teensy 3.2](https://www.pjrc.com/teensy/) microcontroller. The pcb was updated for different buttons spacing, SMD leds and mounting for Teensy.
 
-There are actually two projects in this repo: **Sparknome** which is based on four [Sparkfun PCBs](https://www.sparkfun.com/products/8033), and **Trellinome** based on four [Adafruit Trellis boards](https://www.adafruit.com/product/1616). At the moment of writing both boards cost less than 10USD (per board), and both require some soldering (Sparkfun boards need more than Adafruit). I was able to get Trellinome fully working, and Sparknome semi-working due to soldering problems.
+This is not a replacement for official monome grid (which has much nicer hardware, and supports variable brightness), but just a DIY project.
 
-![](assets/boards.jpeg)
+## itsagrid 64/128
 
-**Sparknome** on right, **Trellinome** on left
+itsagrid is a fork of the Trellinome portion of the [DIY monome project](https://github.com/szymonkaliski/diy-monome) by szymonkaliski.
 
-These boards are not a replacement for official monome build (that is much nicer hardware, and supports variable brightness), but just a DIY project.
+Adjacent pcbs are connected via the SDA, SCL, GND, 5V and INT pads on each board  ([learn.adafruit.com/adafruit-trellis-diy-open-source-led-keypad](https://learn.adafruit.com/adafruit-trellis-diy-open-source-led-keypad)). Four pcbs are used for 64 grid, eight for a 128 grid. Each pcb as a set of three jumpers that need to be configured for unique addressing of the driver chips.
 
-## Sparknome
-
-Sparknome is based on Arduino Mega 2560 due to huge amount of digital pins used (that could be fixed with multiplexer though). I followed official [assembly guide](https://learn.sparkfun.com/tutorials/button-pad-hookup-guide), connecting four boards by joining the adjacent rows and columns. I used RGB LEDs, but wired only single colour (connecting other legs to GND), since monome doesn't support RGB.
-
-The Arduino code to drive the display is in [Sparknome](Sparknome) folder.
-
-## Trellinome
-
-Trellinome was simpler to assemble than Sparknome, because there's much less wiring. It's also harder to make a mistake soldering here. I connected adjacent boards by connecting the SDA, SCL, GND, 5V and INT pins ([learn.adafruit.com/adafruit-trellis-diy-open-source-led-keypad](https://learn.adafruit.com/adafruit-trellis-diy-open-source-led-keypad)).
-
-Since this boards require only few pins to be controlled, I could use smaller Arduino - I went with Pro Mini 5V.
-
-The Arduino code to drive the display is in [Trellinome](Trellinome) folder.
 
 ## Arduino & Hardware & Mext
 
-Recent Monomes use `mext` protocol to communicate OSC with serial data: [Mext Serial Codes](Mext%20Serial%20Codes.txt). Few years ago there was a "Arduinome" project, but it uses pre-mext protocol, and is not compatible with most Max/MSP patches for example. Luckily I found initial implementation made by **TheKitty**: [TheKitty/Untz_Monome](https://github.com/TheKitty/Untz_Monome) that I took as a starting point and build up on.
-
-The official software to communicate with monomes and set up OSC server that other patches connect to is called [`serialoscd`](https://github.com/monome/serialosc). Monome detection is done through FTDI label, there are two solutions to this: either reflash Arduino FTDI chip (I didn't try that, but it's possible), or hack around this in `libmonome` code. There's folder with [patches](Patches) for both `serialosc` project (that just makes it compile on recent macOS), and a patch for `libmonome` that allowed me to detect the Arduino, and make it communicate with external software. You can get both `serialosc` and `libmonome` code from [monome github page](https://github.com/monome), and building them is well documented on official linux docs (they work for macOS as well), read part *2 Preparing your system: serialosc* (ignoring the `sudo apt-get` - I was missing `liblo`, but it's available on homebrew): [monome.org/docs/linux/](https://monome.org/docs/linux/).
-
-I had problems with `serialoscd` that would stop responding when it was flooded with quick messages (for example famous [Meadowphysics](https://monome.org/docs/app/package/) would do that). To work around that I built my own simple nodejs based replacement (that only works with single 8x8 grid): [node-serialoscd](https://github.com/szymonkaliski/node-serialoscd). You need to `yarn install` and then run it with `node index.js /dev/tty...` (where `/dev/tty...` is mext-compatible device TTY).
-
-The `node-serialoscd` was tested with: `Monome Home.maxpat`, `Meadowphysics.maxpat`, `re:mix` and [`node-serialosc`](https://github.com/dinchak/node-serialosc) on macOS 10.13.2.
-
-## Case
-
-![](assets/case.png)
-
-To complete the project I designed simple case using [@jscad/csg](https://github.com/jscad/csg.js) and [three.js](https://threejs.org), and 3D printed it. The exported STLs are part of the repo: [Case/data/](Case/data/), but you can also run the code for yourself (look at the `scripts` section of `package.json`).
+See [szymonkaliski's github](https://github.com/szymonkaliski/diy-monome) for more technical details of his build figuring out the mext/OSC communication.
 
 
+## serialosc / libmonome
+
+You can get both `serialosc` and `libmonome` code from [monome github page](https://github.com/monome), and building them is well documented on official linux docs (they work for macOS as well), read part *2 Preparing your system: serialosc* (ignoring the `sudo apt-get` - I was missing `liblo`, but it's available on homebrew): [monome.org/docs/linux/](https://monome.org/docs/linux/).
+
+
+## Case / Panel
+
+Prototype case is laser cut acrylic top and bottom panels and some spacers. Laser files will be posted here.
--- Trellinome/Trellinome.ino
+++ Trellinome/Trellinome.ino
@@ -1,29 +1,16 @@
-/*------------------------------------------------------------------------
-  Based on code for  Adafruit UNTZtrument - a Trellis button controller.
-  https://github.com/adafruit/Adafruit_UNTZtrument
-  
-  ------------------------------------------------------------------------*/
-  
 #include "Adafruit_Trellis.h"
 
 #define NUM_TRELLIS (4)
 #define NUM_KEYS    (NUM_TRELLIS * 16)
-#define NUM_COLS    (NUM_TRELLIS * 2)
-#define NUM_ROWS    (NUM_TRELLIS * 2)
 
 Adafruit_Trellis matrixes[] = {
-  Adafruit_Trellis(),
-  Adafruit_Trellis(),
-  Adafruit_Trellis(),
-  Adafruit_Trellis(),
   Adafruit_Trellis(),
   Adafruit_Trellis(),
   Adafruit_Trellis(),
   Adafruit_Trellis()
 };
 
-Adafruit_TrellisSet trellis = Adafruit_TrellisSet(&matrixes[0], &matrixes[1], &matrixes[2], &matrixes[3],
-													&matrixes[4], &matrixes[5], &matrixes[6], &matrixes[7]);
+Adafruit_TrellisSet trellis = Adafruit_TrellisSet(&matrixes[0], &matrixes[1], &matrixes[2], &matrixes[3]);
 
 String deviceID  = "monome";
 String serialNum = "m1000000";
@@ -35,80 +22,46 @@ static bool ledBuffer[8][8];
 
 // these functions are from Adafruit_UNTZtrument.h
 static const uint8_t PROGMEM
-	i2xy64[] = { // Remap 8x8 TrellisSet button index to column/row
-		0x00, 0x10, 0x20, 0x30, 0x01, 0x11, 0x21, 0x31,
-		0x02, 0x12, 0x22, 0x32, 0x03, 0x13, 0x23, 0x33,
-		0x40, 0x50, 0x60, 0x70, 0x41, 0x51, 0x61, 0x71,
-		0x42, 0x52, 0x62, 0x72, 0x43, 0x53, 0x63, 0x73,
-		0x04, 0x14, 0x24, 0x34, 0x05, 0x15, 0x25, 0x35,
-		0x06, 0x16, 0x26, 0x36, 0x07, 0x17, 0x27, 0x37,
-		0x44, 0x54, 0x64, 0x74, 0x45, 0x55, 0x65, 0x75,
-		0x46, 0x56, 0x66, 0x76, 0x47, 0x57, 0x67, 0x77 },
-	i2xy128[] = { // Remap 16x8 TrellisSet button index to column/row
-		0x00, 0x10, 0x20, 0x30, 0x01, 0x11, 0x21, 0x31,
-		0x02, 0x12, 0x22, 0x32, 0x03, 0x13, 0x23, 0x33,
-		0x40, 0x50, 0x60, 0x70, 0x41, 0x51, 0x61, 0x71,
-		0x42, 0x52, 0x62, 0x72, 0x43, 0x53, 0x63, 0x73,
-		0x80, 0x90, 0xA0, 0xB0, 0x81, 0x91, 0xA1, 0xB1,
-		0x82, 0x92, 0xA2, 0xB2, 0x83, 0x93, 0xA3, 0xB3,
-		0xC0, 0xD0, 0xE0, 0xF0, 0xC1, 0xD1, 0xE1, 0xF1,
-		0xC2, 0xD2, 0xE2, 0xF2, 0xC3, 0xD3, 0xE3, 0xF3,
-		0x04, 0x14, 0x24, 0x34, 0x05, 0x15, 0x25, 0x35,
-		0x06, 0x16, 0x26, 0x36, 0x07, 0x17, 0x27, 0x37,
-		0x44, 0x54, 0x64, 0x74, 0x45, 0x55, 0x65, 0x75,
-		0x46, 0x56, 0x66, 0x76, 0x47, 0x57, 0x67, 0x77,
-		0x84, 0x94, 0xA4, 0xB4, 0x85, 0x95, 0xA5, 0xB5,
-		0x86, 0x96, 0xA6, 0xB6, 0x87, 0x97, 0xA7, 0xB7,
-		0xC4, 0xD4, 0xE4, 0xF4, 0xC5, 0xD5, 0xE5, 0xF5,
-		0xC6, 0xD6, 0xE6, 0xF6, 0xC7, 0xD7, 0xE7, 0xF7 },
-	xy2i64[8][8] = { // Remap [row][col] to Trellis button/LED index
-		{  0,  1,  2,  3, 16, 17, 18, 19 },
-		{  4,  5,  6,  7, 20, 21, 22, 23 },
-		{  8,  9, 10, 11, 24, 25, 26, 27 },
-		{ 12, 13, 14, 15, 28, 29, 30, 31 },
-		{ 32, 33, 34, 35, 48, 49, 50, 51 },
-		{ 36, 37, 38, 39, 52, 53, 54, 55 },
-		{ 40, 41, 42, 43, 56, 57, 58, 59 },
-		{ 44, 45, 46, 47, 60, 61, 62, 63 }},
-	xy2i128[8][16] = {
-		{  0,  1,  2,  3, 16, 17, 18, 19, 32, 33, 34, 35, 48, 49, 50, 51 },
-		{  4,  5,  6,  7, 20, 21, 22, 23, 36, 37, 38, 39, 52, 53, 54, 55 },
-		{  8,  9, 10, 11, 24, 25, 26, 27, 40, 41, 42, 43, 56, 57, 58, 59 },
-		{ 12, 13, 14, 15, 28, 29, 30, 31, 44, 45, 46, 47, 60, 61, 62, 63 },
-		{ 64, 65, 66, 67, 80, 81, 82, 83, 96, 97, 98, 99,112,113,114,115 },
-		{ 68, 69, 70, 71, 84, 85, 86, 87,100,101,102,103,116,117,118,119 },
-		{ 72, 73, 74, 75, 88, 89, 90, 91,104,105,106,107,120,121,122,123 },
-		{ 76, 77, 78, 79, 92, 93, 94, 95,108,109,110,111,124,125,126,127 } 
-};  
-
+i2xy64[] = {
+  0x00, 0x10, 0x20, 0x30, 0x01, 0x11, 0x21, 0x31,
+  0x02, 0x12, 0x22, 0x32, 0x03, 0x13, 0x23, 0x33,
+  0x40, 0x50, 0x60, 0x70, 0x41, 0x51, 0x61, 0x71,
+  0x42, 0x52, 0x62, 0x72, 0x43, 0x53, 0x63, 0x73,
+  0x04, 0x14, 0x24, 0x34, 0x05, 0x15, 0x25, 0x35,
+  0x06, 0x16, 0x26, 0x36, 0x07, 0x17, 0x27, 0x37,
+  0x44, 0x54, 0x64, 0x74, 0x45, 0x55, 0x65, 0x75,
+  0x46, 0x56, 0x66, 0x76, 0x47, 0x57, 0x67, 0x77
+},
+xy2i64[8][8] = {
+  {  0,  1,  2,  3, 16, 17, 18, 19 },
+  {  4,  5,  6,  7, 20, 21, 22, 23 },
+  {  8,  9, 10, 11, 24, 25, 26, 27 },
+  { 12, 13, 14, 15, 28, 29, 30, 31 },
+  { 32, 33, 34, 35, 48, 49, 50, 51 },
+  { 36, 37, 38, 39, 52, 53, 54, 55 },
+  { 40, 41, 42, 43, 56, 57, 58, 59 },
+  { 44, 45, 46, 47, 60, 61, 62, 63 }
+};
 
 uint8_t xy2i(uint8_t x, uint8_t y) {
-	if (x > 7 || y > 7) {
-		return 255;
-	}
-	if(NUM_KEYS == 64) {
-		if(x > 7) return 255;
-		return pgm_read_byte(&xy2i64[y][x]);
-	} else {
-		if(x > 15) return 255;
-		return pgm_read_byte(&xy2i128[y][x]);
-	}
+  if (x > 7 || y > 7) {
+    return 255;
+  }
 
-//  return pgm_read_byte(&xy2i64[y][x]);
+  return pgm_read_byte(&xy2i64[y][x]);
 }
 
 void i2xy(uint8_t i, uint8_t *x, uint8_t *y) {
-	if (i > NUM_KEYS) {
-		*x = *y = 255;
-		return;
-	}
-	// uint8_t xy = pgm_read_byte(&i2xy64[i]);
-	uint8_t xy = pgm_read_byte((NUM_KEYS == 64) ? &i2xy64[i] : &i2xy128[i]);
-	*x = xy >> 4;
-	*y = xy & 15;
-}
+  if (i > NUM_KEYS) {
+    *x = *y = 255;
+    return;
+  }
 
-// --------
+  uint8_t xy = pgm_read_byte(&i2xy64[i]);
+
+  *x = xy >> 4;
+  *y = xy & 15;
+}
 
 void setLED(int x, int y, int v) {
   if (x >= 0 && x < 8 && y >= 0 && y < 8) {
@@ -135,18 +88,12 @@ void turnOnLEDs() {
 }
 
 void setup() {
-	Serial.begin(115200);
-	// adjust this if x/y makes no sense on your grid
-	
-	if (NUM_KEYS == 64 ){
-		// 2x2 arrangement - addr is the I2C address of the upper left, upper right, lower left and lower right matrices, respectively
-		trellis.begin(0x71, 0x70, 0x73, 0x72);
-	} else if (NUM_KEYS == 128 ){
-		// 4x2 arrangement - 128
-		trellis.begin(0x70, 0x71, 0x72, 0x73, 0x74, 0x75, 0x76, 0x77);
-	}
+  Serial.begin(115200);
 
-	setAllLEDs(0);
+  // adjust this if x/y makes no sense on your grid
+  trellis.begin(0x71, 0x70, 0x73, 0x72);
+
+  setAllLEDs(0);
 }
 
 uint8_t readInt() {
@@ -257,10 +204,10 @@ void processSerial() {
 
       if (readY != 0) break;                  // since we only have 8 LEDs in a column, no offset
 
-      for (y = 0; y < NUM_COLS; y++) {               // each i will be a row
+      for (y = 0; y < 8; y++) {               // each i will be a row
         intensity = readInt();                // read one byte of 8 bits on/off
 
-        for (x = 0; x < NUM_ROWS; x++) {             // for 8 LEDs on a row
+        for (x = 0; x < 8; x++) {             // for 8 LEDs on a row
           if ((intensity >> x) & 0x01) {      // set LED if the intensity bit is set
             setLED(readX + x, y, 1);
           }
@@ -293,7 +240,7 @@ void processSerial() {
 
       if (readY != 0) break;                  // we only have 8 lights in a column
 
-      for (i = 0; i < NUM_COLS; i++) {               // for the next 8 lights in column
+      for (i = 0; i < 8; i++) {               // for the next 8 lights in column
         if ((intensity >> i) & 0x01) {        // if intensity bit set, light
           setLED(readX, i, 1);
         }
@@ -334,8 +281,8 @@ void processSerial() {
       readX = readInt();
       readY = readInt();
 
-      for (y = 0; y < NUM_COLS; y++) {
-        for (x = 0; x < NUM_ROWS; x++) {
+      for (y = 0; y < 8; y++) {
+        for (x = 0; x < 8; x++) {
           if ((x + y) % 2 == 0) {             // even bytes, use upper nybble
             intensity = readInt();
 
@@ -362,7 +309,7 @@ void processSerial() {
       readX = readInt();
       readY = readInt();
 
-      for (x = 0; x < NUM_ROWS; x++) {
+      for (x = 0; x < 8; x++) {
         intensity = readInt();
 
         if (intensity) {
@@ -378,7 +325,7 @@ void processSerial() {
       readX = readInt();
       readY = readInt();
 
-      for (y = 0; y < NUM_COLS; y++) {
+      for (y = 0; y < 8; y++) {
         intensity = readInt();
 
         if (intensity) {
@@ -428,8 +375,8 @@ void loop() {
   }
   else if (now - prevWriteTime >= 10) {
     // set trellis internal matrix from ledBuffer
-    for (uint8_t x = 0; x < NUM_ROWS; x++) {
-      for (uint8_t y = 0; y < NUM_COLS; y++) {
+    for (uint8_t x = 0; x < 8; x++) {
+      for (uint8_t y = 0; y < 8; y++) {
         if (ledBuffer[x][y]) {
           trellis.setLED(xy2i(x, y));
         }
