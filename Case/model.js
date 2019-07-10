d86764d363ffa94908652bd21aec8ce6e1865dac
Szymon Kaliski <hi@szymonkaliski.com>
2019-07-10 17:00:36 +0200

    model fixes

--- Case/model.js
+++ Case/model.js
@@ -13,11 +13,11 @@ const SIZE = USE_ENV
   : [16, 8];
 
 // in CM
-const MONOME_SIZE = [SIZE[0] * 1.5, SIZE[1] * 1.5];
-const PLATE_MARGIN = 0.6;
-const PLATE_HEIGHT = 0.2;
+const PLATE_MARGIN = 0.7;
+const PLATE_HEIGHT = 0.3;
+const MONOME_SIZE = [SIZE[0] * (1 + 0.5) - 0.5, SIZE[1] * (1 + 0.5) - 0.5];
 
-const TopPlate = () => (
+const Plate = () => (
   <cube
     radius={[
       MONOME_SIZE[0] / 2 + PLATE_MARGIN,
@@ -29,39 +29,9 @@ const TopPlate = () => (
   />
 );
 
-const MonomeButtons = () => (
-  <>
-    {range(SIZE[0]).map(i =>
-      range(SIZE[1]).map(j => {
-        const size = 1;
-        const x = -MONOME_SIZE[0] / 2 + size / 2 + 0.2 + i * (size + 0.5);
-        const y = -MONOME_SIZE[1] / 2 + size / 2 + 0.2 + j * (size + 0.5);
-
-        return (
-          <cube key={`${i}-${j}`} radius={size / 2} center={[x, size / 2, y]} />
-        );
-      })
-    )}
-  </>
-);
-
-const MonomeScrewes = () => (
-  <>
-    {range(SIZE[0] / 2).map(i =>
-      range(SIZE[1] / 2).map(j => {
-        const r = 0.2;
-        const x = -MONOME_SIZE[0] / 2 + 1.45 + i * 3;
-        const y = -MONOME_SIZE[1] / 2 + 1.45 + j * 3;
-
-        return <cylinder radius={r / 2} start={[x, 0, y]} end={[x, 1, y]} />;
-      })
-    )}
-  </>
-);
-
 const PlateScrewes = () => {
   const r = 0.4;
-  const offset = (PLATE_MARGIN - r) / 2 + 0.05;
+  const offset = PLATE_MARGIN / 2;
 
   return (
     <>
@@ -80,10 +50,40 @@ const PlateScrewes = () => {
   );
 };
 
+const MonomeButtons = () => (
+  <>
+    {range(SIZE[0]).map(i =>
+      range(SIZE[1]).map(j => {
+        const size = 1;
+        const x = -MONOME_SIZE[0] / 2 + size / 2 + i * (size + 0.5);
+        const y = -MONOME_SIZE[1] / 2 + size / 2 + j * (size + 0.5);
+
+        return (
+          <cube key={`${i}-${j}`} radius={size / 2} center={[x, size / 2, y]} />
+        );
+      })
+    )}
+  </>
+);
+
+const MonomeScrewes = () => (
+  <>
+    {range(SIZE[0] / 2).map(i =>
+      range(SIZE[1] / 2).map(j => {
+        const r = 0.2;
+        const x = -MONOME_SIZE[0] / 2 + 1.25 + i * 3;
+        const y = -MONOME_SIZE[1] / 2 + 1.25 + j * 3;
+
+        return <cylinder radius={r / 2} start={[x, 0, y]} end={[x, 1, y]} />;
+      })
+    )}
+  </>
+);
+
 module.exports = () => (
   <Model showParts={false}>
     <subtract>
-      <TopPlate />
+      <Plate />
 
       {RENDER_TOP && <MonomeButtons />}
       {RENDER_TOP && <MonomeScrewes />}
